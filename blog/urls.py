15598e9cb1b44bf5be6a43c35b4aa8921cf2bb5d
Anas Karab <anaslegend99@gmail.com>
2025-10-03 14:59:40 +0300

    Added sitemap.xml for SEO

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,7 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
     path('build', views.build, name='build'),
-        ]
+
+    # هنا نضيف الـ sitemap
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
+]
